4f18bf7c709f99b8f9ea1a26efb99c016edf5df0
Ryan <silvallucena02@email.com>
2024-02-14 18:48:13 -0300

    Arquivo

---
+++ Nlw Expert/src/http/server.ts
@@ -0,0 +1,28 @@
+import fastfy from 'fastify'
+import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-polls'
+import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
+
+
+
+const app = fastfy()
+
+app.register(cookie, {
+  secret: "polls-app-nlw", 
+  hook: 'onRequest', 
+})
+
+app.register(websocket)
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+app.register(pollResults)
+
+
+app.listen({port:3333}).then(() => {
+    console.log('HTTP server running')
+})
